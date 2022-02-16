82abea190b4fe58d9361bf0395b17968978fa343
JavierR97 <javierrivera.webdev@gmail.com>
2022-02-16 15:33:48 -0500

    added endpoints and users

--- express/server.js
+++ express/server.js
@@ -1,6 +1,7 @@
 const express = require('express'),
   bodyParser = require('body-parser'),
   morgan = require('morgan');
+  uuid = require('uuid')
 
 const app = express();
 
@@ -80,39 +81,97 @@ let movies = [
 var users = [
   
 {
-    name:"john"
+    id:"0",
+    name:"john",
+    favoriteMovies:['Get Out']
 },
 {
-    name:"ted"
+    id:"1",
+    name:"ted",
+    favoriteMovies:[]
 }
 ];
 
+// get all users 
 app.get('/users', (req, res) => {
   res.json(users);
 });
 
-app.post('/users', (req, res) => {
-  const newUser = req.body;
+// get user by id 
+app.get('/users/id/:id', (req, res) => {
+  res.json(users.find((user) =>
+    { return user.id === req.params.id }));
+});
 
-  if (!newUser.name) {
-    const message = 'Missing name in request body';
-    res.status(400).send(message);
+//create a new user
+app.post('/users/newUser', (req, res) => {
+   const newUser = req.body;
+
+    if (newUser.name) {
+      newUser.id = uuid.v4();
+      users.push(newUser);
+      res.status(201).json(newUser);
+    } else {
+      res.status(400).send('users need names');
+    }
+});
+
+// update user by id
+app.put('/users/update/:id', (req, res) => {
+  const id = req.params.id;
+  const updatedUser = req.body;
+
+  let user = users.find(user => user.id === id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
   } else {
-    users.push(newUser);
-    res.status(201).send(newUser);
+    res.status(400).send("no such user");
   }
 });
 
+// add movie to users favorites by user id
+app.post('/users/update/:id/:title', (req, res) => {
+  const id = req.params.id;
+  const title = req.params.title;
 
-app.delete('/users/:name', (req, res) => {
+  let user = users.find(user => user.id === id);
+
+  if (user) {
+    user.favoriteMovies.push(title)
+    res.status(200).send(title + 'has been added to ' + id + "'s array");
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+// delete movie from users favorites by user id
+app.delete('/users/update/:id/:title', (req, res) => {
+  const id = req.params.id;
+  const title = req.params.title;
+
+  let user = users.find(user => user.id === id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter( title => title != title)
+    res.status(200).send(title + ' has been removed from ' + id + "'s array");
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+// delete user by name
+app.delete('/users/deleteUser/:name', (req, res) => {
   let user = users.find((user) => { return user.name === req.params.name });
 
   if (user) {
     users = users.filter((obj) => { return obj.name !== req.params.name });
     res.status(201).send('user ' + req.params.name + ' was deleted.');
+  } else {
+    res.status(400).send('no such user');
   }
 });
-
 // users end
 
 app.get('/', (req, res) => {
@@ -129,36 +188,30 @@ app.get('/documentation', (req, res) => {
   res.sendFile('public/documentation.html', {root: __dirname});
 });
 
-// Gets the list of data about ALL movies
-/* '/movies' is the url endpoint ex. 
-localhost:8080/movies
-
-res.json returns the data within your variable called
-'movies' */
-
+// gets all movies
 app.get('/movies', (req, res) => {
   res.json(movies);
 });
 
-// Gets the data about a single movie, by name
-
+// Gets the data about a single movie, by title
 app.get('/movies/title/:title', (req, res) => {
   res.json(movies.find((movie) =>
     { return movie.title === req.params.title }));
 });
 
+// gets movies by director
 app.get('/movies/director/:director', (req, res) => {
   res.json(movies.find((movie) =>
     { return movie.director === req.params.director }));
 });
 
+// gets movies by genre 
 app.get('/movies/genre/:genre', (req, res) => {
   res.json(movies.filter((movie) =>
     { 
       return movie.genre === req.params.genre;
     }));
 });
-
 
 // Adds data for a new movie to our list.
 app.post('/movies', (req, res) => {
@@ -173,6 +226,7 @@ app.post('/movies', (req, res) => {
   }
 });
 
+// delete movie by title
 app.delete('/movies/:title', (req, res) => {
   let movie = movies.find((movie) => { return movie.title === req.params.title });
 
